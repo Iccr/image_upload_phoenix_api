6065b328eef1f0fa94b6f0e6615b735e1b2b1961
ccr <sis.ccr@gamil.com>
2020-12-03 23:00:45 +0545

    added ecto arc

---
+++ config/config.exs
@@ -0,0 +1,35 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+config :app,
+  ecto_repos: [App.Repo]
+
+# Configures the endpoint
+config :app, AppWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "f07oOrCPRtY+KaJDFp0hwzi2Bu3SYPglf6JYGtDPXq+pz7z+dpigUHzgh72uoNlb",
+  render_errors: [view: AppWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: App.PubSub,
+  live_view: [signing_salt: "hEbn6zAN"]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
+
+config :arc,
+  # or Arc.Storage.Local
+  storage: Arc.Storage.Local
